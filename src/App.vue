4db490391bee671b6249d6647c9dce6c30d1807c
AlexDerAndros <ntrikos@gmx.de>
2023-02-25 18:17:43 +0100

    z

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,26 @@
   Hallo typ ther fggeh
 </div>
 </template>
+<!--
+  um changes hochzuladen musst du das hochladen:
 
+  crt + s auch speichern genannt
+  cd michael
+  npm run build
+  firebase deploy
+
+  um zu github hochzuladen:
+
+  crt + s auch speichern genannt
+  du clicks auf quellencodeverwaltung also die 3 linien mit drei kugeln
+  und clicks auf commit und dann sync changes, also click einfach durch
+
+  * DAS BITTE NICHT LÖSCHEN!!
+-->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
